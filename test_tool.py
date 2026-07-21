116105c906c2ba015f6eab63d34a363c20199811
Anjali Sharma <anjalidecodes@gmail.com>
2026-07-21 14:16:24 +0530

    Converted calculator into LangChain tool

--- test_tool.py
+++ test_tool.py
@@ -1,6 +1,3 @@
 from tools.calculator import calculator
 
-print(calculator("25 * 67"))
-print(calculator("100 / 5"))
-print(calculator("5 + 9"))
-print(calculator("25**"))
+print(calculator.invoke({"expression": "25 * 67"}))
--- tools/calculator.py
+++ tools/calculator.py
@@ -1,16 +1,18 @@
-def calculator(expression: str):
-    """
-    Evaluate a mathematical expression.
+from langchain_core.tools import tool
 
+@tool
+def calculator(expression: str) -> str:
+    """
+    Perform mathematical calculations.
+
+    Input should be a valid mathematical expression.
     Example:
     25 * 67
     """
 
     try:
         result = eval(expression)
-
-        return f"Result: {result}"
+        return str(result)
 
     except Exception as e:
-
         return f"Error: {e}"
